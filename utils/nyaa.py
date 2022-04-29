69a524ac243873a47f5c00ee5a20b08f90b11571
Anshul Garg <garganshul553@gmail.com>
2022-04-29 08:51:44 +0530

    Update nyaa.py

--- utils/nyaa.py
+++ utils/nyaa.py
@@ -3,38 +3,41 @@ from bs4 import BeautifulSoup as bs
 
 
 async def get_torrent(code):
-  r = cloudscraper.create_scraper()
-  x = r.get(f"https://nyaa.si/view/{code}")
-  s = bs(x.content, "html.parser")
-  title = s.find_all("h3", attrs={"class":"panel-title"})[0]
-  link = s.find_all("a", attrs={"class": "card-footer-item"})[0].get("href")
-  d1 = s.find_all("div", attrs={"class":"col-md-1"})
-  d2 = s.find_all("div", attrs={"class":"col-md-5"})
-  title = str(title.string)
-  title = title[4:]
-  inf = []
-  for t in d1:
-    num = d1.index(t)
-    t = t.string
-    t = t[:-1]
-    y = d2[num]
-    if y.span:
-      y = y.span.string
-    elif y.a:
-      y = y.a.string
-    else:
-      y = y.string
-    sq = [t, y]
-    inf.append(sq)
-    try:
-      if "Information" in t:
-        inf.remove([t, y])
+  try:
+    r = cloudscraper.create_scraper()
+    x = r.get(f"https://nyaa.si/view/{code}")
+    s = bs(x.content, "html.parser")
+    title = s.find_all("h3", attrs={"class":"panel-title"})[0]
+    link = s.find_all("a", attrs={"class": "card-footer-item"})[0].get("href")
+    d1 = s.find_all("div", attrs={"class":"col-md-1"})
+    d2 = s.find_all("div", attrs={"class":"col-md-5"})
+    title = str(title.string)
+    title = title[4:]
+    inf = []
+    for t in d1:
+      num = d1.index(t)
+      t = t.string
+      t = t[:-1]
+      y = d2[num]
+      if y.span:
+        y = y.span.string
+      elif y.a:
+        y = y.a.string
       else:
+        y = y.string
+      sq = [t, y]
+      inf.append(sq)
+      try:
+        if "Information" in t:
+          inf.remove([t, y])
+        else:
+          pass
+      except:
         pass
-    except:
-      pass
-  graph = {}
-  for a in inf:
-    graph[f'{a[0]}'] = a[1]
-  graph['magnet'] = link 
-  return graph
+    graph = {}
+    for a in inf:
+      graph[f'{a[0]}'] = a[1]
+    graph['magnet'] = link 
+    return graph
+  except Exception:
+    return {}
